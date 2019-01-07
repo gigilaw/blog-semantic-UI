ad47090838ff56e9560a588baf7a22408d008fab
gigilaw <gigi.law.14@gmail.com>
2019-01-07 18:12:41 +0800

    Index route

--- app.js
+++ app.js
@@ -5,22 +5,37 @@ let mongoose = require("mongoose");
 
 //APP CONFIG
 mongoose.connect(
-  "mongodb://localhost/blog_app",
+  "mongodb://localhost/restful_blog_app",
   { useNewUrlParser: true }
 );
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//MONGOOSE MODEL CONFIG
 let blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
-  created: { tpye: Date, default: Date.now }
+  created: { type: Date, default: Date.now }
 });
 
-//MONGOOSE MODEL CONFIG
-var blog = mongoose.model("Blog", blogSchema);
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server's up");
